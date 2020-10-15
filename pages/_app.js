e360dbb19d4b4eb57e8babe41dd03d0b533f48be
Olohundare Nasirudeen <naskkographics@gmail.com>
2020-10-15 17:49:51 +0100

    updates.

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,6 @@
-import Router from "next/router";
-import NProgress from "nprogress"; //nprogress module
-import "nprogress/nprogress.css"; //styles of nprogress
-import "../public/progress.css";
-import "../public/antd-custom.less";
 import Head from "next/head";
+import "../public/antd-custom.less";
 
-//Binding events.
-Router.events.on("routeChangeStart", () => NProgress.start());
-Router.events.on("routeChangeComplete", () => NProgress.done());
-Router.events.on("routeChangeError", () => NProgress.done());
 function MyApp({ Component, pageProps }) {
   return (
     <>
@@ -19,10 +11,6 @@ function MyApp({ Component, pageProps }) {
           href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
           crossOrigin="anonymous"
-        />
-        <link
-          rel="stylesheet"
-          href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
         />
       </Head>
       <Component {...pageProps} />
